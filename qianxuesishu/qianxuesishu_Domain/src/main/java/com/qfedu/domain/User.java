302e9d174f6b9138395294aa1236a2116a0b3c77
Insect001 <839647050@qq.com>
2018-07-27 17:08:07 +0800

    user

---
+++ qianxuesishu/qianxuesishu_Domain/src/main/java/com/qfedu/domain/User.java
@@ -0,0 +1,19 @@
+package com.qfedu.domain;
+
+/**
+ * @ Author     ：Insect
+ * @ Date       ：Created in 17:00 2018/7/27
+ * @ Description：
+ * @ Modified By：
+ */
+public class User {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
